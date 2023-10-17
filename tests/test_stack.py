829e5b262a0675911f72c643c25111b9da193bad
Levon Kharajyan <l.kharajyan@gmail.com>
2023-10-17 20:04:53 +0300

    stack testing

---
+++ tests/test_stack.py
@@ -0,0 +1,24 @@
+import pytest
+from stack import stack
+
+
+def test_stack():
+    stack.append('one')
+    stack.append('two')
+
+    assert stack.pop() == 'two'
+    assert stack.pop() == 'one'
+
+
+def test_emptiness():
+    assert not stack
+    stack.append('one')
+    assert bool(stack)
+
+    stack.pop()
+    assert not stack
+
+
+def test_pop_with_empty_stack():
+    with pytest.raises(IndexError):
+        stack.pop()
